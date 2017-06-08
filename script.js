bf657af0ca19148e594ec20422d7f964bac79458
Kari Barry <kari.a.barry@stonybrook.edu>
2017-06-08 02:11:51 -0400

    Lots of work implementing a video segment and ensuring cross browser compatibility, unfortunately I've had to give up on Bootstrap for anything other than the side nav entirely; as it can never have a transparent background. Even other HTML objects CAN'T appear on top of A-Frame, so now it's time to resort to embedding everything inside as A-Frame objects...

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
+var videoSp
 $(function () {
+    //Written by Kari Bary on June Seventh Twenty Seventeen
+    //Wait for all the assets to come in, then jump to the assetsLoaded
     document.querySelector( 'a-assets' ).addEventListener( 'loaded', assetsLoaded )
 
 });
 
 function assetsLoaded () {
+  videoSp = $('#DJvideo').get(0);
+
   $("#loader").remove();
-  
+
+  $("#menu li a").on("click", function (evt) {
+            var selected = evt.target.closest("li");
+            $("#menu li").removeClass("active");
+            $("#" + selected.id).attr("class", "active");
+            //console.log($("#MusicBtn").hasClass("active"));
+            setMode();
+
+        });
+
+}
+
+function setMode () {
+  if($("#MusicBtn").hasClass("active")){
+    //videoSp.play()
+    if (!videoSp.paused) {
+      console.log(videoSp.paused)
+      $("a-videosphere").attr('visible', "true");
+      $("#renderedObjects").attr('visible', "false");
+      $("a-sky").attr('visible', "false");
+    }
+    else {
+      $("a-sky").attr("src", "#skyBkp");
+      $("#3dObjects").attr('visible', "false");
+    }
+
+  }
+  else{
+    $("a-videosphere").attr('visible', "false");
+    $("a-sky").attr("src", "#sky");
+    $("a-sky").attr('visible', "true");
+    $("#renderedObjects").attr('visible', "true");
+  }
 }
